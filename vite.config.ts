61d1aa7ee76a7baaa51861c5159b01293d399f1a
陈亮 <17716151441@189.cn>
2023-10-16 18:00:17 +0800

    chore: 代码规范相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
+// src目录绝对路径
+const srcPath = path.resolve(__dirname, 'src')
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+
+export default defineConfig((): UserConfig => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      // 路径别名
+      alias: {
+        '@': srcPath, // src
+      },
+    },
+  }
 })
